a83a2fc0d9ee7c750956c175784654361e621f49
zhangyueban <1374954815@qq.com>
2020-05-25 20:58:35 +0800

    修改登录模块

--- backend/manage.py
+++ backend/manage.py
@@ -15,6 +15,9 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import BadSignature, SignatureExpired
 from passlib.apps import custom_app_context
 from db import get_db
+from flask_login import LoginManager
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash
 
 def create_app():
     app = Flask(__name__)
@@ -31,7 +34,8 @@ def create_app():
     import db
     db.init_app(app)
 
-    #app.add_url_rule("/", endpoint="index")
+    login_manager = LoginManager()
+    login_manager.init_app(app)
 
     return app
 
@@ -40,54 +44,42 @@ auth = HTTPBasicAuth()
 app.app_context().push()  # 推送应用上下文环境
 db = get_db()
 
+class User(UserMixin):
+    pass
 
-class Admin():
-    # 密码加密
-    def hash_password(self, password):
-        self.password = custom_app_context.encrypt(password)
-
-    # 密码解析
-    def verify_password(self, password):
-        return custom_app_context.verify(password, self.password)
-
-    # 获取token，有效时间10min
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
-        return s.dumps({'id': self.id})
-
-    # 解析token，确认登录的用户身份
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            return None  # valid token, but expired
-        except BadSignature:
-            return None  # invalid token
-        admin = Admin.query.get(data['id'])
-        return admin
-
-
+auth = HTTPBasicAuth()
 @auth.verify_password
 def verify_password(name_or_token, password):
     if not name_or_token:
         return False
     name_or_token = re.sub(r'^"|"$', '', name_or_token)
-    admin = Admin.verify_auth_token(name_or_token)
+    admin = None #获取token
     if not admin:
-        admin = Admin.query.filter_by(name=name_or_token).first()
-        if not admin or not admin.verify_password(password):
+        db = get_db()
+        sql = "select name, password from admins where name ='{}'".format(name_or_token)
+        cursor = db.cursor()
+        cursor.execute(sql)
+        user = cursor.fetchall()[0]
+        admin = user[0]
+        password_hash = user[1]
+        print(custom_app_context.verify(password, password_hash))
+        if not admin or not custom_app_context.verify(password, password_hash):
             return False
     g.admin = admin
     return True
 
+def generate_auth_token(expiration=600):
+    s = Serializer(app.config['SECRET_KEY'], expires_in=expiration)
+    return s.dumps({'name': g.admin})
+
+
 
 @app.route('/api/login', methods=['POST'])
-@auth.login_required
+@auth.login_required()
 def get_auth_token():
-    token = g.admin.generate_auth_token()
-    return jsonify({'code': 200, 'msg': "登录成功", 'token': token.decode('ascii'), 'name': g.admin.name})
+    token = generate_auth_token()
+    print(token)
+    return jsonify({'code': 200, 'msg': "登录成功", 'token': token, 'name': g.admin})
 
 
 if __name__ == '__main__':
